ff0662a59c95d7524912f83ee580e8196b2105c7
Megan Sears <megan.sears@colostate.edu>
2026-04-30 14:42:01 -0600

    adding height to the test

--- tests/testthat/test-prepMRMS.R
+++ tests/testthat/test-prepMRMS.R
@@ -1,12 +1,18 @@
 
 
-prepMRMS(dir = './tests/testthat',
-         output_dir = './tests/testthat',
-         num_cores =2,
-         boundary = './tests/testthat/catchments_all_lidar.shp')
+prepMRMS(dir = '/Users/megansears/Documents/MRMS_temp/height21',
+         output_dir = '/Users/megansears/Documents/MRMS_temp/height21_processed',
+         num_cores = 8,
+         boundary = '/Volumes/MSears_Mac2/Documents/MRMS/bbox_2fires/bbox_2fires.shp')
 
 library(terra)
 
 test <- rast('./tests/testthat/RadarOnly_QPE_01H_00.00_20250101-020000_processed.tif')
 
 plot(test)
+
+library(terra)
+library(mapview)
+test1 <- vect('/Volumes/MSears_Mac2/Documents/MRMS/bbox_2fires/bbox_2fires.shp')
+
+mapview(test1)
